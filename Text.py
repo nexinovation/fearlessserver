049fb48f0d5c001a01c098615c8ee5f8d6c59359
fearlessserver <senatoreniola@gmail.com>
2022-12-31 12:35:00 +0100

    Create Text.py

---
+++ Text.py
@@ -0,0 +1,29 @@
+#initiate the variables
+dream = " "
+city = " "
+money = " "
+carbrand = " "
+gender = " "
+familyhouse = " "
+
+
+#ask the user to specify value 
+dream = input(" What do you dream to become? : ")
+city = input(" Which city do you live? : ")
+money = input(" How much to you want? : ")
+carbrand = input(" Enter car brand? : ")
+gender = input(" Are you a man or a woman? : ")
+familyhouse = input(" Where do your family live? : ")
+
+
+#story :p1
+print("\nOnes upon a time, ")
+print("A young " + gender.upper() + " was dreaming of being, " + dream.upper() + ".")
+print("But in is early 1980s, he wasn't getting the big part he wanted.")
+print("he was broke and discouraged, he drove is " + carbrand.upper() + " to the top of a hill,")
+print("overlooking the city of " + city.upper() + " and did nothing unusual. ")
+print("He wrote himself a check of " + money.upper() + " for Acting services rendered. ")
+#p2
+print("\nThis young " + gender.upper() + " had grown up so poor his family lives in " + familyhouse.upper() + " at a time.")
+print("he put his check in his wallet and keep it there. ")
+print("When thing got though, he'd pull it out and look at it to remain himself of his dream. ")
